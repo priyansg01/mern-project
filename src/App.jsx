13b6e4090081dacb74ad7f6dd56ad3c272075b0e
priyansg01 <145562513+priyansg01@users.noreply.github.com>
2024-09-20 12:36:52 +0530

    create pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import Home from './pages/Home'
+import About from './pages/About'
+import {BrowserRouter ,Routes,Route} from 'react-router-dom' 
+import Signin from './pages/Signin'
+import Profile from './pages/Profile'
+import Signout from './pages/Signout'
 
 export default function App() {
-  return (
-    <div><h1 className='text-red-300'>mu nasdna</h1></div>
-  )
+  return <BrowserRouter>
+    <Routes>
+       <Route path='/' element={<Home/>}></Route>
+       <Route path='/about' element={<About/>}></Route>
+       <Route path='/signin' element={<Signin/>}></Route>
+       <Route path='/signout' element={<Signout/>}></Route>
+       <Route path='/profile' element={<Profile/>}></Route>
+    </Routes>
+
+  </BrowserRouter>
 }
