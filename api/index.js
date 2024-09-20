1069a0e194cd20da1df23b84ea38105770005490
priyansg01 <145562513+priyansg01@users.noreply.github.com>
2024-09-20 15:15:19 +0530

    connect to datbase

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 import express from 'express'
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 const app=express();
+dotenv.config();
 
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('db is connnected');
+}).catch((err)=>{
+  console.log(err);
+})
 
 app.listen(3000,()=>{
     console.log('server is listen to port no 3000')
