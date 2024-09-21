80152b68a47d19e2f17cbe485f9a2accecab2622
priyansg01 <145562513+priyansg01@users.noreply.github.com>
2024-09-21 10:53:52 +0530

    complete sign in functionality

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Link,Navigate, useNavigate} from 'react-router-dom'
 
 export default function Signin() {
+    const [formdata,setformdata]=useState({});
+    const [error,seterror]=useState(null);
+    const [loading,setloading]=useState(false);
+    const navigate=useNavigate();
+
+    const handlechange=(e)=>{
+        setformdata({
+            ...formdata,
+            [e.target.id]:e.target.value,//corresponding email username ppassword set
+
+        })
+    }
+    const handlesubmit=async(e)=>{
+        e.preventDefault();
+        try {
+                     setloading(true);
+                     const response=await fetch('/api/auth/signin',{method:'POST',
+                                                               headers:{'Content-Type':'application/json',},body: JSON.stringify(formdata),//form data ko server par bhej rahe h
+                                                             });
+                                                         
+             
+                 const data= await response.json();//wait from the message from server that user is cretaed
+                 if(data.success==false)
+                 {
+                     seterror(data.message);
+                     setloading(false);
+                     return;
+                 }
+                 setloading(false);
+                 seterror(null);
+                 navigate('/');
+                 console.log(data)
+            
+        } catch (error) {
+
+            setloading(false);
+            seterror(error.message)
+            
+        }
+                                }
+  
+
+
   return (
-    <div>Sign-in</div>
-  )
+    <div className='p-3 max-w-lg mx-auto'>
+        <h1 className='text-3xl font-semibold text-center my-7'>Sign In</h1>
+        <form className='flex flex-col gap-4' onSubmit={handlesubmit}>
+            <input type="email" name="" id="email" placeholder='Email' className='border p-3 rounded-lg' onChange={handlechange}/>
+            <input type="password" name="" id="password" placeholder='Password' className='border p-3 rounded-lg' onChange={handlechange}/>
+            <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading?'Loading...':'Sign in'}</button>
+        </form>
+        <div className='flex gap-2 mt-5 '>
+            <p>Don't Have an account?</p>
+            <Link to={'/signup'}>
+               <span className='text-blue-700'>Sign up</span>
+            </Link>
+        </div>
+
+        {error && <p className='text-red-500 mt-5'>{error}</p>}
+    </div>
+  );
 }
