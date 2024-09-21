437b639f541a7d2b01c4d3fdf1c5b1ada0f4fee1
priyansg01 <145562513+priyansg01@users.noreply.github.com>
2024-09-21 10:42:06 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import user from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
 
 export const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
@@ -9,6 +10,33 @@ export const signup=async(req,res,next)=>{
     try{
         await  newuser.save()
         res.status(201).json("user created successfully")
+    }
+    catch(err){
+        next(err)
+    }
+}
+
+export const signin=async(req,res,next)=>{
+    const {email,password}=req.body;
+   
+    try{
+        const validuser=await user.findOne({email})
+        if(!validuser) return next(errorHandler(404,'user not found'));
+        const validpassword=bcryptjs.compareSync(password,validuser.password);
+        if(!validpassword) return next(errorHandler(401,'invalid password'));
+        
+        //save the id or email in cookie token
+        const token=jwt.sign({id:validuser._id},process.env.JWT_SECRET)
+
+        const {password:pass,...rest}=validuser._doc//remove the password or not send again to the user or browser;
+
+        res.cookie('accesstoken',token,{httpOnly:true/*no third party see our cookie*/,maxAge:24*60*60*1000}).status(200).json(rest);
+
+
+
+
+
+       
     }
     catch(err){
         next(err)
