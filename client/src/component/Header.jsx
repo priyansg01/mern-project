37a8777158b635b3d37e7ede7579ad4a1d7476e1
priyansg01 <145562513+priyansg01@users.noreply.github.com>
2024-10-01 12:08:05 +0530

    complete header search form functionality

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {FaSearch} from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link ,useNavigate } from 'react-router-dom'
 import {useSelector} from 'react-redux'
 
 export default function Header() {
   const {currentUser} = useSelector(state=>state.user)
+  const [searchTerm,setsearchTerm]=useState('')
   //console.log(currentuser)
+
+   const Navigate=useNavigate();
+   const handleSubmit=(e)=>{
+     e.preventDefault();
+     const urlParams=new URLSearchParams(window.location.search);
+
+     //window.location gives the current window all properties in object
+     //in window.location has the search in which all the search parameters and gets the parmaters from the url after the ? mark   these is called query parameters  or url parameters or urls variables  so there is a function in js called  URLSearchParams
+ 
+    urlParams.set('searchTerm',searchTerm)
+    //URLSearchParams.set() Sets the value associated with a given search parameter to the given value. If there are several values, the others are deleted.
+
+    const searchQuery=urlParams.toString();  //because some is string and some is number
+    Navigate(`/search?${searchQuery}`)
+   }
+    
+
+   useEffect(()=>{
+    const urlParams=new URLSearchParams(window.location.search);
+     const searchTermFromurl=urlParams.get('searchTerm')
+
+     if(searchTermFromurl)
+     {
+      setsearchTerm(searchTermFromurl)
+     }
+   },[location.search])
+
+
+
+
   return (
     <header className='bg-slate-200 shadow-md'>
         
@@ -17,9 +48,13 @@ export default function Header() {
            </h1>
           </Link>
 
-        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-            <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-            <FaSearch className='text-slate-600'/>
+        <form  onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
+            <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' value={searchTerm} onChange={(e)=>setsearchTerm(e.target.value)}/>
+
+            <button> 
+             <FaSearch className='text-slate-600'/>
+
+            </button>
 
         </form>
         <ul className='flex gap-4 '>
